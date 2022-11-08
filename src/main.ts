8be03bd31a8abf22bf11e280bd0cd03a0b363e74
Chris Atkin <atkinchris@gmail.com>
2022-11-08 22:45:52 +0000

    Begin to cut holes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { primitives, booleans } from '@jscad/modeling'
+import { primitives, booleans, transforms } from '@jscad/modeling'
 import { Geom3 } from '@jscad/modeling/src/geometries/types'
 import { Vec3 } from '@jscad/modeling/src/maths/vec3'
 import stlSerializer from '@jscad/stl-serializer'
@@ -13,6 +13,8 @@ const writeStl = (model: Geom3, fileName: string) => {
 
 const toVec3 = (v: Vect): Vec3 => [v.x, v.y, v.z]
 
+const degToRad = (deg: number) => deg * (Math.PI / 180)
+
 const main = () => {
   const triangles = generatePyramids()
   const points = triangles.flatMap(t => t.map(toVec3))
@@ -20,7 +22,26 @@ const main = () => {
 
   const hull = primitives.polyhedron({ points, faces })
   const innerSphere = primitives.sphere({ radius: 0.9, segments: 64 })
-  const model = booleans.subtract(hull, innerSphere)
+
+  const cylinder = primitives.cylinder({ height: 4, radius: 0.1, segments: 16 })
+
+  // const holeCount = 2
+  // const angleOfSeparation = degToRad(360 / holeCount)
+
+  // const baseHoles = Array.from({ length: holeCount }, (_, i) => transforms.rotateX(angleOfSeparation * i, cylinder))
+  // const holes = Array.from({ length: holeCount }, (_, i) => transforms.rotateY(angleOfSeparation * i, baseHoles))
+
+  const holes = [
+    transforms.rotateY(0, cylinder),
+    transforms.rotateY(degToRad(21), cylinder),
+    transforms.rotateY(degToRad(39.5), cylinder),
+    transforms.rotateY(degToRad(58), cylinder),
+    transforms.rotateY(degToRad(73.5), cylinder),
+    transforms.rotateY(degToRad(90), cylinder),
+    transforms.rotateY(degToRad(106.5), cylinder),
+  ]
+
+  const model = booleans.subtract(hull, innerSphere, holes)
 
   writeStl(model, 'output.stl')
 }
