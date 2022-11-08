a903a4585ca1edb201e7e67fc8c62c89632dcdb3
Chris Atkin <atkinchris@gmail.com>
2022-11-08 22:00:53 +0000

    Rename variables appropriately

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,19 @@ import { Vec3 } from '@jscad/modeling/src/maths/vec3'
 import stlSerializer from '@jscad/stl-serializer'
 import fs from 'fs'
 import generatePyramids from './pyramids'
+import { Vect } from './vect'
 
 const writeStl = (model: Geom3, fileName: string) => {
   const rawData: string[] = stlSerializer.serialize({ binary: false }, model)
   fs.writeFileSync(fileName, rawData.join('\n'))
 }
 
+const toVec3 = (v: Vect): Vec3 => [v.x, v.y, v.z]
+
 const main = () => {
-  const pyramids = generatePyramids()
-  const points = pyramids.flatMap(t => t.map(v => [v.x, v.y, v.z] as Vec3))
-  const faces = pyramids.map((_, i) => [0 + i * 3, 1 + i * 3, 2 + i * 3])
+  const triangles = generatePyramids()
+  const points = triangles.flatMap(t => t.map(toVec3))
+  const faces = triangles.map((_, i) => [0 + i * 3, 1 + i * 3, 2 + i * 3])
 
   const hull = primitives.polyhedron({ points, faces })
   const innerSphere = primitives.sphere({ radius: 0.9, segments: 64 })
