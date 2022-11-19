7a084a2e544eda7ba62d6fe9570c85f6a427558b
Chris Atkin <atkinchris@gmail.com>
2022-11-19 16:24:07 +0000

    Update type override for modelling library

--- src/types/@jscad/modeling.d.ts
+++ src/types/@jscad/modeling.d.ts
@@ -2,8 +2,7 @@ import '@jscad/modeling'
 
 declare module '@jscad/modeling' {
   namespace measurements {
-    declare function measureDimensions(
-      ...geometries: RecursiveArray<Geometry>
-    ): [number, number, number] | [number, number, number][]
+    declare function measureDimensions(geometry: Geometry): [number, number, number]
+    declare function measureDimensions(...geometries: RecursiveArray<Geometry>): [number, number, number][]
   }
 }
