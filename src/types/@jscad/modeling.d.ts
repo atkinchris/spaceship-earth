7fc1043033bf76aa17e281a8df35486b8cd13f85
Chris Atkin <atkinchris@gmail.com>
2022-11-30 10:13:06 +0000

    Upgrade jscad to bring in type fixes

--- src/types/@jscad/modeling.d.ts
+++
@@ -1,8 +0,0 @@
-import '@jscad/modeling'
-
-declare module '@jscad/modeling' {
-  namespace measurements {
-    declare function measureDimensions(geometry: Geometry): [number, number, number]
-    declare function measureDimensions(...geometries: RecursiveArray<Geometry>): [number, number, number][]
-  }
-}
